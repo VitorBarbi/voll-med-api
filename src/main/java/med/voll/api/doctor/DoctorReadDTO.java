d2fed9de2474666123dc4be34bceddcfb7492360
Vitor Barbi <barbi.vitor@gmail.com>
2024-08-25 12:08:35 -0300

    fix: add missing 'phone' field to Doctor read DTO

--- src/main/java/med/voll/api/doctor/DoctorReadDTO.java
+++ src/main/java/med/voll/api/doctor/DoctorReadDTO.java
@@ -1,15 +1,19 @@
 package med.voll.api.doctor;
 
 public record DoctorReadDTO(
+        Long id,
         String name,
         String email,
+        String phone,
         String crm,
         Specialty specialty
 ) {
     public DoctorReadDTO(Doctor doctor){
         this(
+                doctor.getId(),
                 doctor.getName(),
                 doctor.getEmail(),
+                doctor.getPhone(),
                 doctor.getCrm(),
                 doctor.getSpecialty()
         );
